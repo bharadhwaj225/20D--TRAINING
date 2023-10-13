a64cfb5e865d532f3636e8f4ea8f16cebbe57fb4
Bharadhwaj Reddy <116195747+bharadhwaj225@users.noreply.github.com>
2023-10-13 09:32:04 +0530

    Add files via upload

---
+++ 12 - 10 - 2023/OOPC CONCEPT.py
@@ -0,0 +1,14 @@
+class cse:
+    def __init__(self):
+        print("Hi")
+    def fun1(self):
+        print("fun1")
+class aiml(cse):
+    def fun2(self):
+        print("AIML")
+    def fun2(self):
+        print("fun2")
+
+a=aiml()
+a.fun1()
+a.fun2()
